2eda5d51587fedb39e1be17742342a67bc5b2b7d
davechurilla <churilla@gmail.com>
2020-02-15 20:41:17 -0800

    pause

--- src/components/CarouselExplore/_mock.js
+++ src/components/CarouselExplore/_mock.js
@@ -5,5 +5,5 @@ export default {
     backupImg: {
     	url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Tablet-960x469_M75.jpg',
     	alt: 'Lexus Performance'
-    },
+  },
 };
