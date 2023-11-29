b7e243d2453098c0a9be1dac7270d62e16e7b57b
davechurilla <churilla@gmail.com>
2023-11-29 11:51:35 -0800

    update video url

--- src/components/PerformanceHero/_mock.js
+++ src/components/PerformanceHero/_mock.js
@@ -3,34 +3,36 @@ export default {
     // children: 'This is the Performance Hero Component',
     headline: 'PERFORMANCE BORN OF REFINEMENT',
     subheadline: 'The Lexus High-Performance Line',
-    heroVideo: 'https://www.lexus.com/media/performance/PerformanceHeader1',
+    heroVideo:
+      'https://www.lexus.com/content/dam/lexus/videos/performance/LexusHomePage_IS+RCF_1400x580_111222',
     backupImg: {
-    	url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Tablet-960x469_M75.jpg',
-    	alt: 'Lexus Performance'
+      url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Tablet-960x469_M75.jpg',
+      alt: 'Lexus Performance',
     },
     responsiveImages: {
-  	  tabletImg: {
-  	  	url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Tablet-960x469_M75.jpg',
-  	  	alt: 'Tablet image'
-  		},
-  		mobileImg: {
-  			url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Mobile-640x700_M75.jpg',
-  			alt: 'Mobile image'
-  		}
+      tabletImg: {
+        url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Tablet-960x469_M75.jpg',
+        alt: 'Tablet image',
+      },
+      mobileImg: {
+        url: 'https://www.lexus.com/cm-img/performance/landing/05/Lexus-Performance-Hero-Mobile-640x700_M75.jpg',
+        alt: 'Mobile image',
+      },
     },
     ctas: [
-    	 {
-	    	url: 'https://www.lexus.com/media/performance/PerformanceHeader1.mp4',
-	    	text: 'PLAY VIDEO',
-	    	primary: ''
-    	},
-    	 {
-	    	url: '#explore',
-	    	text: 'SEE ALL MODELS',
-	    	primary: 'primary'
-    	}
+      {
+        url: 'https://www.lexus.com/media/performance/PerformanceHeader1.mp4',
+        text: 'PLAY VIDEO',
+        primary: '',
+      },
+      {
+        url: '#explore',
+        text: 'SEE ALL MODELS',
+        primary: 'primary',
+      },
     ],
-    disclaimer: 'Professional driver on a closed course. Do not attempt. Options shown.',
-    options: 'Options shown'
-  }
+    disclaimer:
+      'Professional driver on a closed course. Do not attempt. Options shown.',
+    options: 'Options shown',
+  },
 };
